98e5b5ad61fc5eac17ffce6403cc7ef0dd75ac0c
arhipov035 <arhipov035@gmail.com>
2022-12-25 00:02:22 +0300

    поправил стили поиска

--- components/Search.vue
+++ components/Search.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+  <div class="block p-1 rounded-lg shadow-lg bg-white max-w-sm">
     <form @submit.prevent="searchStore.getProducts(searchProduct)">
-      <div class="form-group mb-6">
-        <label
-          for="exampleInputEmail1"
-          class="form-label inline-block mb-2 text-gray-700"
-          >Email address</label
-        >
+      <div class="form-group">
         <input
           type="text"
           class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
@@ -15,16 +10,12 @@
           placeholder="Поиск"
           v-model="searchProduct"
         />
-        <small id="emailHelp" class="block mt-1 text-xs text-gray-600"
-          >We'll never share your email with anyone else.</small
-        >
       </div>
-
       <button
         type="submit"
         class="px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
       >
-        Submit
+        Найти
       </button>
     </form>
     <Loader v-if="searchStore.loader"/>
@@ -48,3 +39,10 @@ const productStore = useProductStore();
 
 const searchProduct = ref("");
 </script>
+
+<style scoped>
+form{
+  display: flex;
+}
+  
+</style>
